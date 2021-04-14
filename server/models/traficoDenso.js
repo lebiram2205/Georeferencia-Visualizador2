b73a802c0c4e9d949871c39e41818db36fe4cf5a
Hector JS <miguel.lin123@gmail.com>
2021-04-14 18:36:14 -0500

    Commit inicial de la rama dev

--- server/models/traficoDenso.js
+++ server/models/traficoDenso.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const EsquemaTraficoDenso = new Schema({}, { collection: 'tradicodenso2' /*strict:false*/ });
-module.exports = mongoose.model('traficoDenso', EsquemaTraficoDenso);
+const EsquemaTraficoDenso = new Schema({}, { collection: 'traficodenso17' /*strict:false*/ });
+module.exports = mongoose.model('traficodenso', EsquemaTraficoDenso);
